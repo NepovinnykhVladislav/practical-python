81b732e01a83b1bb9708539ca7744a994d9cdfe9
Vladislav Nepovinnykh <42747719+NepovinnykhVladislav@users.noreply.github.com>
2021-02-01 18:12:37 +1000

    Sol 4
    Extra payment option added

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,13 +4,21 @@ principal = 500000
 rate = 0.05
 payment = 2684.11
 total_paid = 0
+month_total = 0
 
-for i in range(12):
-    principal = principal * (1+rate/12) - payment - 1000
-    total_paid = total_paid + payment
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    if month_total >= extra_payment_start_month and month_total <extra_payment_end_month:
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + payment
+        month_total += 1
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        month_total += 1
     
 print('Total paid', total_paid)
+print('Month total', month_total)
