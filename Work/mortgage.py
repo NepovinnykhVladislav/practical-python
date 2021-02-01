2ef29b909c2f3721c11ef8e1f4bd930ac816b71d
Vladislav Nepovinnykh <42747719+NepovinnykhVladislav@users.noreply.github.com>
2021-02-01 18:02:46 +1000

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,9 +1,13 @@
 # mortgage.py
 
-principal = 512000
+principal = 500000
 rate = 0.05
 payment = 2684.11
 total_paid = 0
+
+for i in range(12):
+    principal = principal * (1+rate/12) - payment - 1000
+    total_paid = total_paid + payment
 
 while principal > 0:
     principal = principal * (1+rate/12) - payment
