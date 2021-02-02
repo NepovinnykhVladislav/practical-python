900da812314a53affaa8cb2572bdb0c686e1530a
Vladislav Nepovinnykh <42747719+NepovinnykhVladislav@users.noreply.github.com>
2021-02-02 18:28:22 +1000

    pcost with exception catcher

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    print(f)
+    for line in f:
+        try:
+            row = line.split(',')
+            total_cost += int(row[1]) * float(row[2].strip())
+        except:
+            print('Missing field')
+
+print(f'Total cost {total_cost}')
